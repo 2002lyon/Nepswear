e7ee9e7bb0b877d152f6260b7de69f631604a05b
Leon <leon.lama2002@gmail.com>
2023-12-25 16:14:11 +0545

    category and single page design

--- js/script.js
+++ js/script.js
@@ -8,7 +8,39 @@ const header = document.querySelector("header");
 const storyContainer = document.querySelector(".story-container");
 const headerMid = document.querySelector(".header-mid");
 
-// SLick slider settings
+// responsive navbar on scrolling down
+
+let lastScrollTop = 0;
+function updateHeaderOpacity() {
+  var currentScroll = window.scrollY || window.pageYOffset;
+
+  // if (currentScroll == 0 || lastScrollTop == 0) {
+  //   header.style.position = "relative";
+  // }
+
+  if (currentScroll > lastScrollTop && currentScroll > 100) {
+    header.style.transition = "opacity 0.3s ease";
+    header.style.opacity = "0";
+    // if (storyContainer) {
+    //   storyContainer.style.marginTop = "22rem";
+    // }
+    // header.style.position = "relative";
+  } else {
+    header.style.transition = "opacity 0.3s ease";
+    header.style.position = "fixed";
+    header.style.opacity = "1";
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}
+
+// Initial call to start the animation loop
+requestAnimationFrame(updateHeaderOpacity);
+
+window.addEventListener("scroll", function () {
+  requestAnimationFrame(updateHeaderOpacity);
+});
+
 $(document).ready(function () {
   $(".hero-slide").slick({
     dots: true,
@@ -22,60 +54,12 @@ $(document).ready(function () {
   });
 });
 
-let lastScrollTop = 0;
-window.addEventListener(
-  "scroll",
-  function () {
-    var currentScroll = window.scrollY || window.pageYOffset;
-    if (currentScroll > lastScrollTop) {
-      // headerTop.classList.add("header-top-show");
-      // logoBox.style.display = "none";
-    } else {
-      // headerTop.classList.remove("header-top-show");
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+// function parallax() {
+//   window.onscroll = function () {
+//     var speed = 4.0;
+//     parallexImg.style.backgroundPosition =
+//       -window.scrollX / speed + "px " + -window.scrollY / speed + "px";
+//   };
+// }
 
-    if (window.scrollY > 100) {
-      // logoTop.style.visibility = "visible";
-      header.style.position = "fixed";
-
-      storyContainer.style.marginTop = "22rem";
-      // headerMid.style.display = "none";
-    }
-    if (window.scrollY <= 100) {
-      header.style.position = "relative";
-      storyContainer.style.marginTop = "0rem";
-      // headerMid.style.display = "block";
-    } // For Mobile or negative scrolling
-  },
-  false
-);
-
-var MainImg = document.getElementById("MainImg");
-var smallImg = document.getElementsByClassName("small-img");
-
-smallImg[0].onclick = function () {
-  MainImg.src = smallImg[0].src;
-};
-
-smallImg[1].onclick = function () {
-  MainImg.src = smallImg[1].src;
-};
-
-smallImg[2].onclick = function () {
-  MainImg.src = smallImg[2].src;
-};
-
-smallImg[3].onclick = function () {
-  MainImg.src = smallImg[3].src;
-};
-
-function parallax() {
-  window.onscroll = function () {
-    var speed = 4.0;
-    parallexImg.style.backgroundPosition =
-      -window.scrollX / speed + "px " + -window.scrollY / speed + "px";
-  };
-}
-
-parallax();
+// parallax();
